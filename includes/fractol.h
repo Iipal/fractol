0b139aaf11c3b8b471b7d2fe27d35bac4bc3e9c9
_ipal <malkoleyplay@gmail.com>
2019-01-30 11:41:47 +0200

    minor fixed & added unsigned int typedef(uint)

--- includes/fractol.h
+++ includes/fractol.h
@@ -6,7 +6,7 @@
 /*   By: tmaluh <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/29 14:51:41 by tmaluh            #+#    #+#             */
-/*   Updated: 2019/01/29 15:42:09 by tmaluh           ###   ########.fr       */
+/*   Updated: 2019/01/30 11:35:03 by tmaluh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,23 +25,26 @@
 # include "errno_msg.h"
 # include <mlx.h>
 
-# define WIN_X	2000
+# define WIN_X	1000
 # define WIN_Y	1000
 
-# define MALDEBROT	"Maldebrot"
+# define MANDELBROT	"Mandelbrot"
 # define JULIA		"Julia"
 
 enum	e_bool {false, true};
 
-# define _BOOL typedef enum e_bool bool
+# define _BOOL typedef enum e_bool	bool
+# define _UNIT typedef unsigned int	uint
 
 _BOOL;
+_UNIT;
 
 # define _ISARGS(ac, av, ufunc){--ac;++av;if(ac != 1){ufunc();return(false);}}
 
 # define _MSG(msg) ft_putstr(msg)
 # define _MSGN(msg) ft_putendl(msg)
 # define _NOTIS(msg, ex, ret) if (!(ex)) {_MSGN(msg); return(ret);}
+# define _NOTIS_DO(msg, ex, ret, do) if (!(ex)) {_MSGN(msg);do;return(ret);}
 # define _NOTIS_F(ex) if (!(ex)) return (false)
 
 # define MPTR	fl->mlx->mlx
@@ -54,14 +57,14 @@ typedef struct	s_mlx
 	pvoid	mlx;
 	pvoid	win;
 	pvoid	img;
-	int		*screen;
+	uint	*screen;
 }				t_mlx;
-
 
 typedef struct	s_fl
 {
 	t_mlx	*mlx;
 	string	mode;
+	uint	mnum:4;
 }				t_fl;
 
 int				fl_key_hooks(int key, t_fl *fl);
--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: tmaluh <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/29 14:53:10 by tmaluh            #+#    #+#             */
-/*   Updated: 2019/01/29 15:46:00 by tmaluh           ###   ########.fr       */
+/*   Updated: 2019/01/30 11:40:54 by tmaluh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,38 +19,31 @@ static void	usage(void)
 	_MSGN("\t2. Julia");
 }
 
-static bool	add_valid_arg(t_fl *fl, string arg)
-{
-	const char	*modes[] = {MALDEBROT, JULIA};
-	int			mode;
-
-	_NOTIS_F(!(!(mode = ft_atoi(arg)) || mode > 2));
-	_NOTIS_F(fl->mode = ft_strdup(modes[--mode]));
-	return (true);
-}
-
 static bool	add_init_fractol(t_fl *fl)
 {
 	int	bpp;
 	int	sl;
 	int	end;
 
-	*fl = (t_fl){NULL, fl->mode};
+	*fl = (t_fl){NULL, fl->mode, fl->mnum};
 	_NOTIS_F(fl->mlx = malloc(sizeof(t_mlx)));
 	_NOTIS_F(MPTR = mlx_init());
 	_NOTIS_F(WPTR = mlx_new_window(MPTR, WIN_X, WIN_Y, fl->mode));
 	_NOTIS_F(IPTR = mlx_new_image(MPTR, WIN_X, WIN_Y));
-	_NOTIS_F(SPTR = (int*)mlx_get_data_addr(IPTR, &bpp, &sl, &end));
+	_NOTIS_F(SPTR = (uint*)mlx_get_data_addr(IPTR, &bpp, &sl, &end));
 	return (true);
 }
 
 int			main(int argc, cstring argv[])
 {
-	t_fl	*fl;
+	const char	*modes[] = {MANDELBROT, JULIA};
+	t_fl		*fl;
 
 	_ISARGS(argc, argv, usage);
 	_NOTIS(_ERR_ALLOC_MEM_, fl = malloc(sizeof(t_fl)), -1);
-	_NOTIS(_ERR_INVALID_ARGS_, add_valid_arg(fl, (string)*argv), -1);
+	_NOTIS_DO(_ERR_INVALID_ARGS_,
+		!(!(fl->mnum =  ft_atoi((string)*argv)) || fl->mnum > 2), -1, usage());
+	_NOTIS(_ERR_ALLOC_MEM_, fl->mode = ft_strdup(modes[--(fl->mnum)]), -1);
 	_NOTIS(_ERR_ALLOC_MEM_, add_init_fractol(fl), -1);
 	mlx_hook(WPTR, KEY_PRESSED, KEY_RELEASE, fl_key_hooks, fl);
 	mlx_loop(MPTR);
