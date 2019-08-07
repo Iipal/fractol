dd1c5ba44e699fc245d51fcb716e548dccb50746
Iipal <malkoleyplay@gmail.com>
2019-08-07 17:17:10 +0300

    bugfix

--- srcs/fl_refresh_screen.c
+++ srcs/fl_refresh_screen.c
@@ -6,7 +6,7 @@
 /*   By: tmaluh <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/30 12:05:31 by tmaluh            #+#    #+#             */
-/*   Updated: 2019/08/07 11:27:38 by tmaluh           ###   ########.fr       */
+/*   Updated: 2019/08/07 17:16:52 by tmaluh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	fl_refresh_screen(t_fl *fl)
 {
-	const t_fptr_fls	fls[] = {&fl_mandelbrot, &fl_julia, &fl_burn_ship,
+	const t_fptr_fls	fractls[] = {&fl_mandelbrot, &fl_julia, &fl_burn_ship,
 								&fl_juliav2, &fl_juliav3};
 	size_t				i;
 	size_t				res;
@@ -22,9 +22,9 @@ void	fl_refresh_screen(t_fl *fl)
 	i = ~0UL;
 	while (WIN_MATRIX > ++i)
 	{
-		res = fls[fl->mnum - 1](fl, i);
+		res = fractls[fl->mnum - 1](fl, i);
 		if (fl->depth > res)
-			fl->mlx->screen[i] = (res + 1.0 / (i ? i : 1.0) + 1) * fl->color;
+			fl->mlx->screen[i] = (res + (i ? (1 / i) : 1) + 1) * fl->color;
 		else
 			fl->mlx->screen[i] = !fl->color;
 	}
